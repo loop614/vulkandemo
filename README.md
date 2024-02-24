c2fc49a760cc6232a54589de6b75bd99f0e150bf
loop <tony.kusic@gmail.com>
2024-02-24 15:46:18 +0100

    cube rotating

--- README.md
+++ README.md
@@ -7,6 +7,9 @@ After "Vertex buffers" section:
 After "Uniform buffers" section:
 <div align="left"><img src="https://raw.githubusercontent.com/loop614/vulkandemo/main/uniform_buffers.png" width=401 height=316 alt="uniform_buffers"/></div>
 
+Rotate the cube:
+<div align="left"><img src="https://raw.githubusercontent.com/loop614/vulkandemo/main/cube_rotating.gif" width=600 height=338 alt="cube_rotating"/></div>
+
 ## Requirements
 - [Here](https://vulkan-tutorial.com/Development_environment)
 
--- main.cpp
+++ main.cpp
@@ -71,7 +71,7 @@ struct UniformBufferObject {
 };
 
 struct Vertex {
-    glm::vec2 pos;
+    glm::vec3 pos;
     glm::vec3 color;
 
     static VkVertexInputBindingDescription getBindingDescription() {
@@ -88,7 +88,7 @@ struct Vertex {
 
         attributeDescriptions[0].binding = 0;
         attributeDescriptions[0].location = 0;
-        attributeDescriptions[0].format = VK_FORMAT_R32G32_SFLOAT;
+        attributeDescriptions[0].format = VK_FORMAT_R32G32B32_SFLOAT;
         attributeDescriptions[0].offset = offsetof(Vertex, pos);
 
         attributeDescriptions[1].binding = 0;
@@ -101,18 +101,40 @@ struct Vertex {
 };
 
 const std::vector<Vertex> vertices = {
-    {{-0.85f, -0.5f}, {1.0f, 0.0f, 0.0f}},
-    {{-0.1f, -0.5f}, {0.0f, 1.0f, 0.0f}},
-    {{0.1f, -0.5f}, {1.0f, 0.0f, 0.0f}},
-    {{0.85f, -0.5f}, {0.0f, 1.0f, 0.0f}},
-    {{-0.1f, 0.5f}, {0.0f, 0.0f, 1.0f}},
-    {{0.1f, 0.5f}, {1.0f, 1.0f, 1.0f}},
-    {{0.85f, 0.5f}, {0.0f, 0.0f, 1.0f}}
+    {{-0.5f, -0.5f, -0.5f}, {1.0f, 0.0f, 0.0f}},
+    {{0.5f, -0.5f, -0.5f}, {0.0f, 1.0f, 0.0f}},
+    {{0.5f, 0.5f, -0.5f}, {0.0f, 0.0f, 1.0f}},
+    {{-0.5f, 0.5f, -0.5f}, {1.0f, 0.0f, 0.0f}},
+    {{-0.5f, -0.5f, 0.5f}, {0.0f, 1.0f, 0.0f}},
+    {{0.5f, -0.5f, 0.5f}, {0.0f, 0.0f, 1.0f}},
+    {{0.5f, 0.5f, 0.5f}, {1.0f, 0.0f, 0.0f}},
+    {{-0.5f, 0.5f, 0.5f}, {0.0f, 1.0f, 0.0f}},
 };
 
 const std::vector<uint16_t> indices = {
-    0, 1, 4,
-    2, 3, 6, 6, 5, 2
+    // bottom
+    4, 5, 6,
+    6, 7, 4,
+
+    // east
+    5, 6, 2,
+    2, 1, 5,
+
+    // north
+    4, 5, 1,
+    1, 0, 4,
+
+    // west
+    4, 0, 3,
+    3, 7, 4,
+
+    // south
+    3, 2, 6,
+    6, 7, 3,
+
+    // top
+    0, 1, 2,
+    2, 3, 0
 };
 
 
@@ -281,9 +303,8 @@ private:
         float time = std::chrono::duration<float, std::chrono::seconds::period>(currentTime - startTime).count();
         UniformBufferObject ubo{};
         ubo.model = glm::rotate(glm::mat4(1.0f), time * glm::radians(90.0f), glm::vec3(0.0f, 0.0f, 1.0f));
-        ubo.view = glm::lookAt(glm::vec3(2.0f, 2.0f, 2.0f), glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3(0.0f, 0.0f, 1.0f));
+        ubo.view = glm::lookAt(glm::vec3(3.0f, 3.0f, -3.0f), glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3(0.0f, 0.0f, 1.0f));
         ubo.proj = glm::perspective(glm::radians(45.0f), swapChainExtent.width / (float) swapChainExtent.height, 0.1f, 10.0f);
-        ubo.proj[1][1] *= -1;
         memcpy(this->uniformBuffersMapped[currentImage], &ubo, sizeof(ubo));
     }
 
